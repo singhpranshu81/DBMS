d7637ca2f732208068718e18b0b0818226dd59a5
singhpranshu81 <thescholar02@gmail.com>
2024-08-12 10:07:33 +0530

    Update about.sql

--- about.sql
+++ about.sql
@@ -125,3 +125,281 @@ select * from emp where EXTRACT(YEAR FROM hiredate)=1980 and EXTRACT(MONTH FROM 
 select ename,empno,sal*12 as ann from emp where ann between 22000 and 45000;
 -- 20
 select ename from emp where ename like '_____';
+
+-- can't use alias name in where clause but in grpby can use
+--------------------------------------------------------------------
+-- GROUP BY
+
+-- use to create sub groups/categories
+-- creetes unique records
+-- columns in the select list should be available in group by clause 
+select job,ename from emp group by job,ename;  
+
+select job from emp group by job,ename; 
+-- column within the aggregate function in the select list may or may not be in group by clause
+select job,count(ename) from emp group by job;  
+-- have the where clause before grp by
+select job from emp where job='manager' group by job,ename;
+
+select deptno,max(sal) from emp group by deptno;
+-- to put condition in it use having i.e restricting the no.of rows in the grp we use having
+select deptno,max(sal) from emp group by deptno having deptno=20;
+
+select job,max(sal) from emp where max(sal)>3000 group by job,ename;-- don't use aggregate function in where clause below one is correct using having
+
+select deptno,max(sal) from emp group by deptno having max(sal)>3000;
+select deptno,max(sal) from emp group by deptno having sal>3000;   -- this will throw error bcz column in slelct list must be present in grp by (aggregate function can be there like in above clause max())
+select deptno,max(sal) from emp group by deptno,sal having sal>3000;  -- this will work fine
+
+
+-- 1
+select deptno,count(empno) from emp group by deptno;
+
+-- 2
+select job , sum(sal) from emp group by job;
+
+-- 3
+select count(ename) ,EXTRACT(YEAR FROM hiredate) as hp from emp group by hp;
+
+-- 4
+select count(ename) ,mgr from emp group by mgr having mgr is not null;
+
+-- 5
+select count(ename),job,deptno from emp group by job,deptno;
+
+-- 6
+select sum(sal)*12 ,EXTRACT(YEAR FROM hiredate) as hp from emp group by hp;
+
+-- 7
+select avg(sal) ,deptno,job from emp group by deptno,job having job='manager';
+
+--------------------------------------------------------------------------------------------------------
+-- JOINS
+-- joinning more than one table  to retrieve colums from different tables
+--  equi  join or nner join  - a intersection b
+-- non-equi join
+-- outer join --> left right full ouetr join
+-- self join 
+-- cross join
+
+-- 1)equi  join 
+select e.empno,e.ename,e.job,d.deptno,d.dname from emp e join dept d
+ on e.deptno = d.deptno where e.deptno=10;
+-- 2) non-equi join
+select e.empno,e.ename,e.job,d.deptno,d.dname from emp e join dept d
+ on e.deptno=10;
+-- 3) left outer
+select e.empno,e.ename,e.job,d.deptno,d.dname from emp e left join dept d
+ on e.deptno = d.deptno;
+-- 4) right
+select e.empno,e.ename,e.job,d.deptno,d.dname from emp e right join dept d
+ on e.deptno = d.deptno ;
+-- 5)cross join
+select e.empno,e.ename,e.job,d.deptno,d.dname from emp e cross join dept d;
+
+-- self join --  when u have hierarachy use self join
+select m.ename Manager, e.ename Employee from emp as e join emp as m on e.mgr=m.empno; -- employess managaer id is equal to manger employee id go by words
+
+----------
+-- 1
+select m.ename Manager, e.ename Employee from emp as e join emp as m on e.mgr=m.empno;
+
+-- 2
+select e.ename,d.dname from emp as e right join dept as d on e.deptno=d.deptno;
+
+-- 3
+select e.ename,d.dname from emp as e right join dept as d on e.deptno=d.deptno where e.ename is null;
+
+-- 4
+select e.ename Employee,e.job as JOB ,d.dname as dept from emp as e join dept as d on e.deptno=d.deptno;
+-- 5
+select ifnull(SUM(e.sal),0),d.dname from emp as e right join dept as d on e.deptno=d.deptno group by d.dname;
+
+-- 21
+select ename from emp where ename like 'S%' and length(ename)=5;
+-- 22
+select ename from emp where ename like '__r%' and length(ename)=4;
+-- 23
+select ename from emp where ename like 'S%H';
+-- 24
+select ename from emp where extract(month from hiredate)=1;
+
+-- 25
+select ename,HIREDATE from emp where monthname(hiredate) like '_a%';
+
+-- 26
+select ename,sal from emp where length(truncate(sal,0))=4 and truncate(sal,0)%10=0;
+
+-- 27
+select ename from emp where ename like '%ll%' ;
+
+-- 28
+select ename from emp where extract(year from hiredate)=1980;
+-- 29
+select ename from emp where deptno<>20;
+-- 30
+select ename ,sal from emp where job not in('PRESIDENT','MGR') order by sal;
+
+-- 31
+select ename,hiredate from emp where extract(year from hiredate)<1981 or extract(year from hiredate)>1981 ;
+-- 32
+select ename from emp where empno not like '78%';
+
+-- 33
+select ename,mgr from emp where mgr='7839';
+
+-- 34
+select ename,hiredate from emp  where extract(month from hiredate)<>3;
+
+-- 35
+select ename from emp where job='clerk' and deptno=20;
+
+----------------------------------------------------------------------
+-- CONSTRAINTS
+-- 2 types
+-- 1)Column level
+-- 2)table level
+
+-- primary key  --> no dyuplicate no null
+-- unique key --> no duplicate , accept null
+-- not null --. accept duplicates , no null
+-- check  --> it accepts expression.validate the correctness of data
+-- foreign key --> create relationship among table
+-- default value --  if value is not given this will be filled
+
+ 
+ 
+ 
+ 
+ -- TRASACTION (commit ,rollback,savepoint) --> starts with DML ends with TCL(commit, rollback)
+ 
+ -- syntax:
+--  START TRANSACTION   --> this will initiate the transaction
+--  .....
+--  insert insert 
+--  savepoint
+--  update
+--  savepoint
+--  delete
+--  commit;  or roll back --> end the transaction
+--  
+ -- if u rollback/commit, all savepoints will be erased
+ -- after a commit roll back can't happen
+ 
+create table student (
+stid int,
+stname varchar(20),
+dob date not null,
+marks double,
+numb int,
+dept int
+);
+ insert into student values(12,'gg','1998-07-07',85.56,15151,2);
+ START transaction
+ select * from student;
+ insert into student values(13,'bb','1998-08-07',87.56,156151,2);
+    insert into student values(14,'kk','1999-07-07',83.56,14455,1);
+ savepoint A;
+ update student set marks=90.12 where stid=14;
+ 
+ savepoint B;
+ 
+  update student set stname='sfjfj' where stid=14;
+ rollback to A;
+ commit;
+
+-----------------------------------
+-- SUB-QUERY  -- the output of one query is input for other and the inner query will be executed only once (exception correlaed querires), outer query can be executed multiple times.
+-- inner q. is always independent of outer query except co related
+TYPES OF SUB-QUERY:
+-- single-row subq  -> inner query will fetch only one value as result
+-- multi row s.   -> the inner query will fetch many rows
+-- multiple column subq --> the inner query will fetch many rows and columns
+-- co related subq --> inner query gets exe. as many times as outer qu.
+
+
+Single row sub-query
+1) whose salary is great tha ALllen salry?
+select ename, sal from emp where sal>(select sal from emp where ename='ALLEN'); 
+
+2) query to find who is earning highest sal?
+select ename,sal from emp where sal=(select max(sal) from emp );
+3) 2nd highest slary
+select ename,sal from emp 
+where sal=(select max(sal) from  emp where sal<(select max(sal) from emp ));
+
+Multi row subquery
+-- IN , ANY ,ALL
+ANY and ALL will not work as such. It has to prefixed with any of the rlational operator.
+>ANY  --   >MIN
+<ANY  --      <MAX
+=ANY  ---  IN
+>ALL  --  >MAX
+<ALL  --- <MIN
+
+1) write a query to find the salary of employees whose salary is equal to the salary of atleast one employee in deptno 30.
+-> select ename,job,sal from  emp where sal in(select sal from emp where deptno=30);
+
+--> select ename,job,sal from emp where sal=ANY(select sal from emp where deptno=30);
+
+2)Write query to find the salaryies of employees whose salary is greater than slary of atleast one employee in deptno 30.
+
+ select ename,job,sal from emp where sal>ANY(select sal from emp where deptno=30);
+
+ select ename,job,sal from emp where sal<ANY(select sal from emp where deptno=30);
+
+-- 1
+-- select d.dname from dept as d join emp as e on e.deptno=d.deptno where e.sal>ALL(select avg(p.sal) from emp as p group by p.deptno ) group by e.deptno;
+-- select dname from emp where sal=(select avg(sal) from emp)
+
+-- 1
+select d.dname from dept as d join emp as e on e.deptno=d.deptno group by d.dname
+ having avg(e.sal)>(select avg(sal) from emp ) ;
+ 
+
+-- 2
+
+select d.dname from dept as d join emp as e on e.deptno=d.deptno group by d.dname
+ having  max(e.sal)>(select avg(sal) from emp );
+
+-- 3
+select d.dname from dept as d join emp as e on e.deptno=d.deptno
+where e.sal>2900 group by d.dname having count(e.sal)>2;
+
+
+----------------------------------------------------------
+-- Complex joins
+-- retrieve all orders with products and customer details
+select order_id,customer_name,product_name 
+from customers c join orders o on c.customer_id=o.customer_id
+join products p on p.product_id=o.product_id;
+
+-- total number of orders
+select p.product_name ,count(order_id) from orders o join products p on o.product_id=p.product_id group by product_name;
+-- total revenue generated by each customer
+select c.customer_name ,sum(product_price*quantity) as reven
+from customers c join orders o on c.customer_id=o.customer_id 
+join products p on o.product_id=p.product_id 
+group by customer_name limit 1;
+
+-- find the customer who hav not placed ant order
+select c.customer_name 
+from customers c  left join orders o on c.customer_id=o.customer_id 
+-- join products p on o.product_id=p.product_id 
+where o.order_id is null;
+
+
+-- customer names who have placed laptops
+ 
+ select c.customer_name 
+from customers c join orders o on c.customer_id=o.customer_id 
+join products p on o.product_id=p.product_id 
+where p.product_name='Laptop';
+ 
+ 
+ 
+ select * from customers; 
+select * from products;
+select * from Orders;
+ 
+ 
